eb1d6d57eb72b26eb5358cbbdc7d0dcadb828dd8
Joel Smith <71782153+JoelSmi@users.noreply.github.com>
2021-12-01 05:25:07 -0500

    Commenting
    Ensuring the necessary information is in place for the main function of the overall application

--- FDC Routing Algorithms.cpp
+++ FDC Routing Algorithms.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int main()
 {
+	//Graph used throughout the program
 	int initialGraph[Verticies][Verticies]
 	{ { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
 	{ 4, 0, 8, 0, 0, 0, 0, 11, 0 },
@@ -19,24 +20,33 @@ int main()
 	{ 8, 11, 0, 0, 0, 0, 1, 0, 7 },
 	{ 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
 
+	//Queue lengths for each of the node's links within the network
 	int NodeQueues[Verticies]{ 1000, 100, 50, 70, 45, 80, 1000,100,1000};
 	
+	//Treansmission rate from the given nodes
 	int transmissionRate[Verticies]{ 10,8,12,9,7,10,6,7 ,13};
 	bool isStatic = true;
 
+	//Tracking the packet loos for both the static and semi static runs
 	int staticPacketLoss;
 	int SemiStaticPacketLoss;
 
+	//Static sim object creation with message size of 1000 and source 0, destion 6
 	simulation sim = simulation(initialGraph, NodeQueues, transmissionRate, 1000, 0, 6, true);
+	
+	//strat time to determine run time of current simulation
 	int startTime = clock();
 	sim.run();
 	int staticTime = clock() - startTime;
 	staticPacketLoss = sim.DroppedPacket;
 
+	//Semi-static sim object creation with message size of 1000 and source 0, destion 6
 	sim = simulation(initialGraph, NodeQueues, transmissionRate, 1000, 0, 6, false);
 	startTime = clock();
-	//sim.run();
 	sim.run();
+	
+	//CURRENTLY NOT FUNCTIONING
+	//Attempt to multithread the simulation run function
 	/*if (sim.isUpdated) {
 		cout << "Starting Multithreaded processing... " << endl;
 		thread original(run, sim, sim.DR);
@@ -49,6 +59,7 @@ int main()
 	int SemiStaticTime = clock() - startTime;
 	SemiStaticPacketLoss = sim.DroppedPacket;
 
+	//displaying the packet loss and run times for both the static and semi static runs
 	cout << " Static Packet Loss: " << staticPacketLoss << "\tSemi-Static Packet Loss: " << SemiStaticPacketLoss << endl;
 	cout << " Static Completion Time: " << staticTime << "\tSemi-Static Completion Time: " << SemiStaticTime << endl;
 
