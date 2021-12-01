2bdbdd114036bbb0d052d0fffe8636487d98e0c1
Joel Smith <71782153+JoelSmi@users.noreply.github.com>
2021-12-01 05:37:54 -0500

    Commenting
    Commenting to describe the aspects of the SemiStaticRouting.cpp file

--- SemiStaticRouting.cpp
+++ SemiStaticRouting.cpp
@@ -4,33 +4,39 @@
 #include "SemiStaticRouting.h"
 using namespace std;
 
-
-
+//Function to update the graph in the givin Dijkstra routing object based on the source, destination, queues, and the current(initial graph)
 DijkstraRouting UpdateGraph(DijkstraRouting DRObject, int src, int dest, int Queues[Verticies], int CurrentGraph[Verticies][Verticies]) {
 	int tempGraph[Verticies][Verticies];
 	for (int i = 0; i < Verticies; i++) {
 		for (int j = 0; j < Verticies; j++) {
 			if (CurrentGraph[i][j] != 0) {
 				if (searchPath(DRObject.Path[dest], j)) {
+					//Only updates the the graph if the current path contains the j value
 					tempGraph[i][j] = Queues[i] + CurrentGraph[i][j];
 				}
 				else {
+					//copies the value from the currentGraph array to the tempGraph array
 					tempGraph[i][j] = CurrentGraph[i][j];
 				}
 			}
 			else {
+				//Assigning zero to all undifined/0 values in the current graph array
 				tempGraph[i][j] = 0;
 				continue;
 			}
 		}
 	}
+	
+	//calling of the setGraph function in the DRObject found in DijkstraRouting.cpp
 	DRObject.setGraph(tempGraph);
-	DRObject.printGraph();
+	
+	//calling of the overloaded Dijkstra function in the DRObject found in DijkstraRouting.cpp
 	DRObject.Dijkstra(src,dest);
 
 	return DRObject;
 }
 
+//Searches the given path fro a specific value and returns the boolean value of the path search
 bool searchPath(list<int> path, int value) {
 	for (auto it = path.begin(); it != path.end(); it++) {
 		if (*it == value) {
