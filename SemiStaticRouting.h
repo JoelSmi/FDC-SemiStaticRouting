331ecba4ce788531bc2be2ca00ea8833a71cb831
Joel Smith <71782153+JoelSmi@users.noreply.github.com>
2021-12-01 05:30:39 -0500

    commenting
    Simple comments to provide an overview.

--- SemiStaticRouting.h
+++ SemiStaticRouting.h
@@ -1,5 +1,9 @@
 #pragma once
 #include "DijkstraRouting.h" 
 
+//Implementations for the following functions in the SemiStatic.cpp file
+//conrtoller function to update the current paths within the DijkstraRouting objects for ideal pathing based on current snapshot of the graphs and queues
 DijkstraRouting UpdateGraph(DijkstraRouting DRObject, int src, int dest, int QueueLengths[Verticies], int CurrentGraph[Verticies][Verticies]);
+
+//Searcing the current path for a specific value
 bool searchPath(list<int> path, int value);
