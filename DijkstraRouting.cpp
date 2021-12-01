4c179b300fdc73ea004b7dbfaa80988e14cc59b7
Joel Smith <71782153+JoelSmi@users.noreply.github.com>
2021-12-01 05:14:23 -0500

    Commenting
    Added comments describing the function/purpose of the overall DijkstraRouting.cpp file

--- DijkstraRouting.cpp
+++ DijkstraRouting.cpp
@@ -12,6 +12,7 @@
 #include "DijkstraRouting.h"
 using namespace std;
 
+//setGraph function to replace the object's currentGraph with the given graph 2d array 
 void DijkstraRouting::setGraph(int Graph[Verticies][Verticies]) {
 	for (int i = 0; i < Verticies; i++) {
 		for (int j = 0; j < Verticies; j++) {
@@ -22,7 +23,7 @@ void DijkstraRouting::setGraph(int Graph[Verticies][Verticies]) {
 	this->Path->clear();
 }
 
-
+//MinDistance function determines the node with the smallest distance from the source based upon the given sptSet.
 static int MinDistance(int distance[Verticies], bool sptSet[Verticies]) {
 
 	int TempMin = INT_MAX, MinDistance_idx;
@@ -36,6 +37,7 @@ static int MinDistance(int distance[Verticies], bool sptSet[Verticies]) {
 	return MinDistance_idx;
 }
 
+//printGraph function displays the objects currentGraph 2d Array
 void DijkstraRouting::printGraph() {
 	for (int i = 0; i < Verticies; i ++) {
 		for (int j = 0; j < Verticies; j++) {
@@ -45,6 +47,7 @@ void DijkstraRouting::printGraph() {
 	}
 }
 
+//PrintSolution function displays all the nodes and their distances from the source
 void DijkstraRouting::PrintSolution() {
 	cout << "Vertex \t Distance from source" << endl;
 	for (int i = 0; i < Verticies; i++) {
@@ -54,28 +57,39 @@ void DijkstraRouting::PrintSolution() {
 	}
 }
 
+//Dijkstra function binder function reponsible for determining the unprocessed nodes and passing the unprocessed nodes to the MinDistance function
 void DijkstraRouting::Dijkstra(int src)
 {
 	int distance[Verticies];
 	bool sptSet[Verticies];
-
+	
+	//Assigning the distance and sptSet arrays
 	for (int i = 0; i < Verticies; i++) {
 		distance[i] = INT_MAX;
 		sptSet[i] = false;
 	}
-
+	
+	//Ensuring that the source node it added to its own path for accurate path allocation
 	if (this->Path[src].empty()) {
 		distance[src] = 0;
 		this->Path[src].push_back(src);
 	}
 
+	//Controlling loop handling the Mindistance function and the Path Assignments
 	for (int i = 0; i < Verticies; i++) {
 		int Unprocessed = MinDistance(distance, sptSet);
 		sptSet[Unprocessed] = true;
 		for (int j = 0; j < Verticies; j++) {
+			
+			//4 conditions of the if statement
+			//1) checking that the node j has not been assigned proviously
+			//2) Checking to ensure that the link in position (Unprocessed, j) exists
+			//3) Ensures that the distance is less than the maximum int allowed
+			//4) Ensures that the value about to be added to the path/distance array is less than the value in the j position of the distanc earray
 			if (!sptSet[j] && this->ConnectedGraph[Unprocessed][j] && distance[Unprocessed] != INT_MAX && distance[Unprocessed] + ConnectedGraph[Unprocessed][j] < distance[j]) {
 				distance[j] = distance[Unprocessed] + this->ConnectedGraph[Unprocessed][j];
-
+				
+				//j-value is the node to be assigned while Unprocessed-value is the path on which j is assigned 
 				this->Path[j] = this->Path[Unprocessed];
 				this->Path[j].push_back(j);
 				this->Path[j].unique();
@@ -84,21 +98,24 @@ void DijkstraRouting::Dijkstra(int src)
 			}
 		}
 	}
-
+	
+	//Assigning the local distance array to the gblDistance array
 	for (int i = 0; i < Verticies; i++) {
 		this->gblDistances[i] = distance[i];
 	}
 }
 
+//Overloaded version of the Djkstra function which recieves the destion value and calls the original Dijkstra function
 void DijkstraRouting::Dijkstra(int src, int dest)
 {
 	for (int i = 0; i < Verticies; i++) {
 		this->Path[i].clear();
 	}
-
+	
 	cout << "Soruce: " << src << ";  Destination: " << dest << endl;
 	Dijkstra(src);
 
+	//displaying the Path order to ensure proper flow through the network linnks
 	cout << "Path order";
 	for (auto it = this->Path[dest].begin(); it != this->Path[dest].end(); it++) {
 		cout << "  " << *it;
